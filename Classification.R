bf19b7da266b16a9c9f47dc18354f7e7560fd370
Pooja Phadke <poojaphadke@tamu.edu>
2019-09-23 11:27:56 -0500

    iteration 2

--- Classification.R
+++ Classification.R
@@ -12,7 +12,17 @@ bag.imp1=importance(bag.train1, type=1) #important variables
 bag.imp.order1=order(bag.imp1, decreasing = TRUE) #important variables order (rownumbers)
 
 
-
+#Bagging 2nd iteration
+set.seed(7)
+bag.train2=randomForest(Activity~.-Activity_ID, data = train_data, mtry=20, importance=TRUE )
+summary(bag.train2)
+varImpPlot(bag.train2)
+ypred.bag2=predict(bag.train2, test_data, type="class")
+table(ypred.bag2,test_data$Activity)
+mean(ypred.bag2!=test_data$Activity)
+mean(ypred.bag2!=test_data$Activity)
+bag.imp2=importance(bag.train2, type=1) #important variables
+bag.imp.order2=order(bag.imp2, decreasing = TRUE) #important variables order (rownumbers)
 
 
 
